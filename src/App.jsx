a2631629da1169d2a7f552bcd52bbe82ca0e71b2
hridayroy19 <hrhridoyroy503@gmail.com>
2024-02-06 13:09:20 +0600

    crate a deashbord header setion and design

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+// import { useState } from 'react'
+import { useState } from 'react'
+import './app.css'
+// import Header from './Header'
+// import Sidebar from './Sidebar'
+// import Home from './Home'
+
+import Header from "./component/Header"
+import Home from "./component/Home"
+import Sidebar from "./component/Sidebar"
+
+function App() {
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
+
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle)
+  }
+
+  return (
+    <div className='grid-container'>
+      <Header OpenSidebar={OpenSidebar}/>
+      {/* <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/> */}
+      {/* <Home /> */}
+    </div>
+  )
+}
+
+export default App
